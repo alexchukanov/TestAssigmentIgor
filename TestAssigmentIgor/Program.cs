76a820c717543aaf5ada6282acf477e60089330a
Alex Chukanov <alex.chukanov@gmail.com>
2022-04-26 01:44:15 +0300

    Add project files.

---
+++ TestAssigmentIgor/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAssigmentIgor
+{
+    //Task description:
+    //You’ve an array of numbers [1,2,4,5,6,8].
+    //Please, find “3” and "7" and so on using LINQ.
+    internal class Program
+    {
+        static List<int> list = new List<int> {1, 2, 4, 5, 6, 8};
+        
+        static void Main(string[] args)
+        {
+            int i = 1;
+            list.ForEach(x =>
+                    {
+                        if (x != i)
+                        {
+                            Console.Write(i + " ");
+                            i++;
+                        }                       
+                        i++;                        
+                    });
+            Console.ReadLine();
+        }
+    }
+}
